6e2c51954d0fa03a398cb4d3a874aa9229a9996a
KhiemNguyenViet <khiemnguyenviet.2004@gmail.com>
2025-05-03 23:13:17 +0700

    Khiem initinit

--- includes/config.php
+++ includes/config.php
@@ -11,5 +11,15 @@
 	$tlca_data['dbname'] = 'mercedes';
 
 	date_default_timezone_set('Asia/Saigon');
+
+	 //Connect 
+
 	$conn = mysqli_connect($tlca_data['server'],$tlca_data['dbuser'],$tlca_data['dbpassword'],$tlca_data['dbname']);
+	//mysqli_set_charset($conn,"utf8");
+	// Template config
+
+	//@define(skin_name,'skin'); 
+
+	//@define(skin_ext,'.tpl');
+
 ?>
